621ea58acb35a44651ce114cc5eab1ddf10eb084
Bo Dai <daibo@infini-ai.com>
2025-09-10 13:26:22 +0000

    feat(reshard): refactor pp reshard
    Signed-off-by: Bo Dai <daibo@infini-ai.com>

--- rlinf/utils/resharding/mcore_weight_reshard.py
+++ rlinf/utils/resharding/mcore_weight_reshard.py
@@ -13,10 +13,16 @@
 # limitations under the License.
 
 
+from typing import Dict, List
+
 import torch
 from megatron.core import parallel_state
+from torch import nn
 
-from .reshard_config import ReshardConfig
+from rlinf.utils.convertor.utils import get_mg2hf_convertor
+
+from .reshard_config import ReshardConfig, ReshardWeightFormat
+from .utils import get_pp_resharder, get_tp_resharder
 
 
 class MegatronCoreWeightReshard:
@@ -33,15 +39,59 @@ class MegatronCoreWeightReshard:
             f"Invalid tensor model parallel size {self.config.model_config.tensor_model_parallel_size} "
             f"and reshard tp size {self.config.reshard_tp_size}. "
         )
+
         self.tp_subgroups = {}
         self.merge_factor = (
             self.config.model_config.tensor_model_parallel_size
             // self.config.reshard_tp_size
         )
+        self._init_distribution_config()
         self._create_tp_subgroups()
 
-    def _create_tp_subgroups(self):
+        self._pp_resharder = (
+            get_pp_resharder(model_arch=self.config.model_arch, pp_group=self.pp_group)
+            if self.reshard_pp_model
+            else None
+        )
+        self._tp_resharder = (
+            get_tp_resharder(
+                model_arch=self.config.model_arch, merge_factor=self.merge_factor
+            )
+            if self.reshard_tp_model
+            else None
+        )
+        self._mg2hf_convertor = (
+            get_mg2hf_convertor(
+                model_arch=self.config.model_arch, config=self.config, strict=True
+            )
+            if self.convert_to_hf
+            else None
+        )
+
+    def _init_distribution_config(self):
         self.world_size = torch.distributed.get_world_size()
+        self.tp_size = parallel_state.get_tensor_model_parallel_world_size()
+        self.pp_rank = parallel_state.get_pipeline_model_parallel_rank()
+        self.pp_size = parallel_state.get_pipeline_model_parallel_world_size()
+        self.pp_group = parallel_state.get_pipeline_model_parallel_group()
+        self.vp_size = parallel_state.get_virtual_pipeline_model_parallel_world_size()
+
+        if not self.vp_size:
+            self.vp_size = 1
+
+        self.convert_to_hf = (
+            self.config.reshard_weights_format != ReshardWeightFormat.MCORE
+        )
+        self.reshard_pp_model = self.pp_size > 1 and (
+            self.config.reshard_pp_size != self.pp_size
+            or self.config.reshard_tp_size != self.tp_size
+        )
+        self.reshard_tp_model = True
+        # NOTE (wyq): Always reshard TP model even when tp_size == reshard_tp_size.
+        # When tp_size == reshard_tp_size, resharding is equivalent to copying.
+        # The rollout engine may load incorrect weights if not copied before offloading.
+
+    def _create_tp_subgroups(self):
         num_groups = self.world_size // self.merge_factor
         all_subgroups = [
             list(range(i * self.merge_factor, (i + 1) * self.merge_factor))
@@ -55,10 +105,17 @@ class MegatronCoreWeightReshard:
                     subgroup_ranks, backend="nccl"
                 )
 
-    def _get_tp_subgroup(self, subgroup_ranks):
+    def _get_tp_subgroup(self, rank: int) -> torch.distributed.ProcessGroup:
         """
         Retrieve an existing communication subgroup.
         """
+        group_index = rank // self.merge_factor
+        subgroup_ranks = list(
+            range(
+                group_index * self.merge_factor,
+                (group_index + 1) * self.merge_factor,
+            )
+        )
         key = tuple(subgroup_ranks)
         if key in self.tp_subgroups:
             return self.tp_subgroups[key]
@@ -67,7 +124,7 @@ class MegatronCoreWeightReshard:
             f"Subgroup {key} does not exist! Please call _create_tp_subgroups() to create this subgroup first."
         )
 
-    def gather_and_reshard_model(self, model):
+    def gather_and_reshard_model(self, model: List[nn.Module]) -> Dict:
         """
         Accumulate all vp model chunks together, and reshard model (i.e) gather all pp ranks
         if required and return the final model state dict
@@ -90,32 +147,12 @@ class MegatronCoreWeightReshard:
             layer_index = int(_get_layer_index(split_key))
             return int(split_key[layer_index])
 
-        tp_size = parallel_state.get_tensor_model_parallel_world_size()
-        pp_rank = parallel_state.get_pipeline_model_parallel_rank()
-        pp_size = parallel_state.get_pipeline_model_parallel_world_size()
-        pp_group = parallel_state.get_pipeline_model_parallel_group()
-        vp_size = parallel_state.get_virtual_pipeline_model_parallel_world_size()
-        if not vp_size:
-            vp_size = 1
-
-        reshard_pp_model = False
-        reshard_tp_model = True
-        # NOTE (wyq): Always reshard TP model even when tp_size == reshard_tp_size.
-        # When tp_size == reshard_tp_size, resharding is equivalent to copying.
-        # The rollout engine may load incorrect weights if not copied before offloading.
-        if (
-            self.config.reshard_tp_size != tp_size
-            or self.config.reshard_pp_size != pp_size
-        ):
-            if pp_size > 1:
-                reshard_pp_model = True
-
-        layers_per_pp = self.config.model_config.num_layers // pp_size
-        layers_per_chunk = layers_per_pp // vp_size
+        layers_per_pp = self.config.model_config.num_layers // self.pp_size
+        layers_per_chunk = layers_per_pp // self.vp_size
 
         tl_params = {}
         model_level_params = {}
-        if vp_size > 1:  # consolidate params across model chunks
+        if self.vp_size > 1:  # consolidate params across model chunks
             for idx, model_chunk in enumerate(model):
                 for key, val in model_chunk.state_dict().items():
                     if "_extra_state" in key:
@@ -124,7 +161,8 @@ class MegatronCoreWeightReshard:
                         if "layers" in key:
                             key2 = rename_layer_num(
                                 key,
-                                get_layer_num(key) + idx * pp_size * layers_per_chunk,
+                                get_layer_num(key)
+                                + idx * self.pp_size * layers_per_chunk,
                             )
                             tl_params[key2] = val
                         else:
@@ -139,18 +177,20 @@ class MegatronCoreWeightReshard:
                     else:
                         model_level_params[key] = val
 
-        if vp_size > 1 or reshard_pp_model:
+        if self.vp_size > 1 or self.reshard_pp_model:
             # gather layers across pp ranks
             gathered_params = {}
             for key, val in tl_params.items():
-                weight_list = [torch.zeros_like(val) for _ in range(pp_size)]
-                torch.distributed.all_gather(weight_list, val, group=pp_group)
-                for idx in range(pp_size):
+                weight_list = [torch.zeros_like(val) for _ in range(self.pp_size)]
+                torch.distributed.all_gather(weight_list, val, group=self.pp_group)
+                for idx in range(self.pp_size):
                     layer_num = get_layer_num(key) + idx * layers_per_chunk
                     key2 = rename_layer_num(key, layer_num)
-                    if not reshard_pp_model:  # Save only layers of 1 single PP stage
-                        layers_start = layers_per_pp * pp_rank
-                        layers_end = layers_per_pp * (pp_rank + 1) - 1
+                    if (
+                        not self.reshard_pp_model
+                    ):  # Save only layers of 1 single PP stage
+                        layers_start = layers_per_pp * self.pp_rank
+                        layers_end = layers_per_pp * (self.pp_rank + 1) - 1
                         if layer_num >= layers_start and layer_num <= layers_end:
                             key2 = rename_layer_num(key, layer_num % layers_per_pp)
                             gathered_params[key2] = weight_list[idx]
@@ -161,28 +201,15 @@ class MegatronCoreWeightReshard:
         model_state_dict = model_level_params
         model_state_dict.update(tl_params)
 
-        reshard_dtype = self.config.model_config.params_dtype
+        if self.reshard_pp_model:
+            model_state_dict = self._pp_resharder.apply(model_state_dict)
 
-        if reshard_pp_model:
-            model_state_dict = self.config.pp_reshard_fn(
-                model_state_dict, pp_group, reshard_dtype
-            )
-
-        if reshard_tp_model:
+        if self.reshard_tp_model:
             rank = torch.distributed.get_rank()
-            group_index = rank // self.merge_factor
-            subgroup_ranks = list(
-                range(
-                    group_index * self.merge_factor,
-                    (group_index + 1) * self.merge_factor,
-                )
-            )
-            tp_sub_group = self._get_tp_subgroup(subgroup_ranks)
-            model_state_dict = self.config.tp_reshard_fn(
-                model_state_dict, self.merge_factor, tp_sub_group
-            )
+            tp_subgroup = self._get_tp_subgroup(rank)
+            model_state_dict = self._tp_resharder.apply(model_state_dict, tp_subgroup)
 
-        if self.config.convert_fn is not None:
-            model_state_dict = self.config.convert_fn(model_state_dict)
+        if self.convert_to_hf:
+            model_state_dict = self._mg2hf_convertor.convert(model_state_dict)
 
         return model_state_dict
