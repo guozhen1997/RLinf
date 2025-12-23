509aabc827041effb1d8a01c14e24a913123d3a0
Hao Lin <linhaomails@gmail.com>
2025-12-23 10:28:56 +0000

    feat: adapt collect data to worker and realworld env
    Signed-off-by: Hao Lin <linhaomails@gmail.com>

--- examples/embodiment/collect_real_data.py
+++ examples/embodiment/collect_real_data.py
@@ -17,82 +17,76 @@ import copy
 import os
 import pickle as pkl
 
-import gymnasium as gym
 import hydra
 import numpy as np
 
-from rlinf.envs.realworld.common.wrappers import (
-    GripperCloseEnv,
-    Quat2EulerWrapper,
-    RelativeFrame,
-    SpacemouseIntervention,
-)
-from rlinf.scheduler import Cluster
+from rlinf.envs.realworld.realworld_env import RealworldEnv
+from rlinf.scheduler import Cluster, ComponentPlacement, Worker
+
+
+class DataCollector(Worker):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.cfg = cfg
+        self.success_needed = 20
+        self.total_cnt = 0
+        self.env = RealworldEnv(
+            cfg.env.train, num_envs=1, seed_offset=0, total_num_processes=1
+        )
+
+        self.transitions = []
+
+    def run(self):
+        obs, _ = self.env.reset()
+        success_cnt = 0
+        self.log_info("Start collecting data...")
+        while success_cnt < self.success_needed:
+            action = np.zeros((6,))
+            next_obs, rew, done, truncated, info = self.env.step(action)
+            if "intervene_action" in info:
+                action = info["intervene_action"]
+
+            transition = copy.deepcopy(
+                {
+                    "observations": obs,
+                    "actions": action,
+                    "next_observations": next_obs,
+                    "rewards": rew,
+                    "masks": 1.0 - done,
+                    "dones": done,
+                }
+            )
+            self.transitions.append(transition)
+
+            obs = next_obs
+
+            if done:
+                success_cnt += rew
+                self.total_cnt += 1
+                self.log_info(
+                    f"{rew}\tGot {success_cnt} successes of {self.total_cnt} trials. {self.success_needed} successes needed."
+                )
+                obs, _ = self.env.reset()
+        save_file_path = os.path.join(self.cfg.runner.logger.log_path, "data.pkl")
+        with open(save_file_path, "wb") as f:
+            pkl.dump(self.transitions, f)
+            self.log_info(f"Saved {self.success_needed} demos to {save_file_path}")
+
+        self.env.close()
 
 
 @hydra.main(
     version_base="1.1", config_path="config", config_name="realworld_collect_data"
 )
 def main(cfg):
-    Cluster(cluster_cfg=cfg.cluster)
-
-    success_needed = 20
-    success_cnt = 0
-    total_cnt = 0
-    env = gym.make(
-        "PegInsertionEnv-v1",
-        override_cfg={
-            "robot_ip": "ROBOT_IP",
-            "camera_serials": [
-                "CAMERA_SERIAL_NUMBER",
-            ],
-            "target_ee_pose": "TARGET_EE_POSE_AS_LIST",  # pos and euler angles
-        },
+    cluster = Cluster(cluster_cfg=cfg.cluster)
+    component_placement = ComponentPlacement(cfg, cluster)
+    env_placement = component_placement.get_strategy("env")
+    collector = DataCollector.create_group(cfg).launch(
+        cluster, name=cfg.env.group_name, placement_strategy=env_placement
     )
-
-    env = GripperCloseEnv(env)
-    env = SpacemouseIntervention(env)
-    env = RelativeFrame(env)
-    env = Quat2EulerWrapper(env)
-
-    transitions = []
-
-    obs, _ = env.reset()
-    print("Start collecting data...")
-    while success_cnt < success_needed:
-        action = np.zeros((6,))
-        next_obs, rew, done, truncated, info = env.step(action)
-        if "intervene_action" in info:
-            action = info["intervene_action"]
-
-        transition = copy.deepcopy(
-            {
-                "observations": obs,
-                "actions": action,
-                "next_observations": next_obs,
-                "rewards": rew,
-                "masks": 1.0 - done,
-                "dones": done,
-            }
-        )
-        transitions.append(transition)
-
-        obs = next_obs
-
-        if done:
-            success_cnt += rew
-            total_cnt += 1
-            print(
-                f"{rew}\tGot {success_cnt} successes of {total_cnt} trials. {success_needed} successes needed."
-            )
-            obs, _ = env.reset()
-
-    save_file_path = os.path.join(cfg.runner.logger.log_path, "data.pkl")
-    with open(save_file_path, "wb") as f:
-        pkl.dump(transitions, f)
-        print(f"saved {success_needed} demos to {save_file_path}")
-
-    env.close()
+    collector.run().wait()
 
 
 if __name__ == "__main__":
