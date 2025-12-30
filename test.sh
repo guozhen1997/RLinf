bdb7d8b62c8eff5547c77c992887b8964773b21f
Iron-Wph <27790564355@qq.com>
2025-12-30 16:38:41 +0800

    chore(config): stop tracking local yaml files
    Signed-off-by: Iron-Wph <27790564355@qq.com>

--- test.sh
+++
@@ -1,6 +0,0 @@
-export ROBOTWIN_PATH="/mnt/public/peihong/codes/Robotwin_support"
-
-export ROBOT_PLATFORM=ALOHA # $ROBOT_PLATFORM
-export PYTHONPATH="/mnt/public/peihong/codes/RLinf:/opt/libero:${ROBOTWIN_PATH}:/mnt/public/peihong/codes/RLinf:/opt/libero:$PYTHONPATH"
-python tests/unit_tests/test_robotwin_env.py
-# python tests/unit_tests/test_model.py
