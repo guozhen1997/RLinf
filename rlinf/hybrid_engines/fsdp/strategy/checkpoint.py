1611574a8b7b3e225a1b13e99bde6534522ef88c
guozhen1997 <2997871698@qq.com>
2026-02-10 17:30:35 +0800

    fix: fix sac checkpoint, update docs
    Signed-off-by: guozhen1997 <2997871698@qq.com>

--- rlinf/hybrid_engines/fsdp/strategy/checkpoint.py
+++ rlinf/hybrid_engines/fsdp/strategy/checkpoint.py
@@ -15,11 +15,11 @@
 from collections.abc import Iterable
 from typing import Union
 
+import torch
+
 from torch.distributed.checkpoint.state_dict import (
     StateDictOptions,
-    get_model_state_dict,
     get_state_dict,
-    set_model_state_dict,
     set_state_dict,
 )
 from torch.distributed.checkpoint.stateful import Stateful
@@ -27,7 +27,7 @@ from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LRScheduler
 
 from rlinf.hybrid_engines.fsdp import FSDP, FSDPModule
-from rlinf.hybrid_engines.fsdp.utils import FSDPVersion
+from rlinf.hybrid_engines.fsdp.utils import FSDPVersion, to_local_if_dtensor
 from rlinf.utils.utils import get_rng_state, set_rng_state
 
 
@@ -39,11 +39,10 @@ class Checkpoint(Stateful):
         lr_schedulers: Union[LRScheduler, Iterable[LRScheduler]],
         opts: StateDictOptions,
         fsdp_version: FSDPVersion,
+        checkpoint_format: str = "dcp",
     ):
         self.model = model
-        self.optimizers = (
-            (optimizers,) if isinstance(optimizers, Optimizer) else tuple(optimizers)
-        )
+        self.optimizers = optimizers
         self.lr_schedulers = (
             (lr_schedulers,)
             if isinstance(lr_schedulers, LRScheduler)
@@ -51,36 +50,56 @@ class Checkpoint(Stateful):
         )
         self.opts = opts
         self.fsdp_version = fsdp_version
+        self.checkpoint_format = checkpoint_format
 
-    def _uses_orig_params(self) -> bool:
-        all_handles = getattr(self.model, "_all_handles", None)
-        if not all_handles:
-            return False
-        return any(getattr(handle, "_use_orig_params", False) for handle in all_handles)
+    def _get_local_optim_state_dicts(self):
+        if isinstance(self.optimizers, Optimizer):
+            return self.optimizers.state_dict()
+        return [opt.state_dict() for opt in self.optimizers]
+
+    def _load_local_optim_state_dicts(self, optim_state_dicts):
+        if isinstance(self.optimizers, Optimizer):
+            self.optimizers.load_state_dict(optim_state_dicts)
+        else:
+            for opt, opt_sd in zip(self.optimizers, optim_state_dicts):
+                opt.load_state_dict(opt_sd)
 
     def state_dict(self):
-        use_raw_optim = self._uses_orig_params() and len(self.optimizers) > 1
-        if use_raw_optim:
-            model_sd = get_model_state_dict(model=self.model, options=self.opts)
-            optim_sd = [optim.state_dict() for optim in self.optimizers]
+        if self.checkpoint_format == "local_shard":
+            model_sd = self.model.state_dict()
+            model_sd = {
+                key: to_local_if_dtensor(value).cpu()
+                if isinstance(value, torch.Tensor)
+                else value
+                for key, value in model_sd.items()
+            }
+            optim_sd = self._get_local_optim_state_dicts()
+
+            lr_sched_sd = [lr.state_dict() for lr in self.lr_schedulers]
+
+            out = {
+                "model": model_sd,
+                "optimizers": optim_sd,
+                "lr_schedulers": lr_sched_sd,
+                "fsdp_version": self.fsdp_version.value,
+                "rng": get_rng_state(),
+            }
         else:
             model_sd, optim_sd = get_state_dict(
-                model=self.model, optimizers=self.optimizers, options=self.opts
+                model=self.model,
+                optimizers=self.optimizers,
+                options=self.opts,
             )
-        lr_sched_sd = []
-        for lr_sched in self.lr_schedulers:
-            lr_sched_sd.append(lr_sched.state_dict())
 
-        out = {
-            "model": model_sd,
-            "lr_schedulers": lr_sched_sd,
-            "fsdp_version": self.fsdp_version.value,
-        }
-        if use_raw_optim:
-            out["optimizers_raw"] = optim_sd
-        else:
-            out["optimizers"] = optim_sd
-        out["rng"] = get_rng_state()
+            lr_sched_sd = [lr.state_dict() for lr in self.lr_schedulers]
+
+            out = {
+                "model": model_sd,
+                "optimizers": optim_sd,
+                "lr_schedulers": lr_sched_sd,
+                "fsdp_version": self.fsdp_version.value,
+                "rng": get_rng_state(),
+            }
         return out
 
     def load_state_dict(self, state):
@@ -88,35 +107,27 @@ class Checkpoint(Stateful):
         ckpt_fsdp_version = FSDPVersion(state["fsdp_version"])
         if ckpt_fsdp_version != self.fsdp_version:
             raise ValueError(
-                f"FSDP version mismatch: checkpoint version {ckpt_fsdp_version} != current version {self.fsdp_version}"
+                f"FSDP version mismatch: {ckpt_fsdp_version} != {self.fsdp_version}"
             )
-        if "optimizers_raw" in state:
-            set_model_state_dict(
-                model=self.model,
-                model_state_dict=state["model"],
-                options=self.opts,
-            )
-            for optim, optim_sd in zip(self.optimizers, state["optimizers_raw"]):
-                optim.load_state_dict(optim_sd)
+
+        if self.checkpoint_format == "local_shard":
+            self.model.load_state_dict(state["model"])
+
+            self._load_local_optim_state_dicts(state["optimizers"])
+
         else:
             set_state_dict(
                 model=self.model,
                 optimizers=self.optimizers,
                 model_state_dict=state["model"],
-                optim_state_dict=state["optimizers"]
-                if "optimizers" in state
-                else state["optim"],
+                optim_state_dict=state.get("optimizers", state.get("optim")),
                 options=self.opts,
             )
-        if self.lr_schedulers is not None:
-            if "lr_schedulers" in state:
-                for lr_sched, lr_sched_sd in zip(
-                    self.lr_schedulers, state["lr_schedulers"]
-                ):
-                    lr_sched.load_state_dict(lr_sched_sd)
-            elif "lr_scheduler" in state:
-                lr_sched_sd = [state["lr_scheduler"]]
-                for lr_sched, lr_sched_sd in zip(self.lr_schedulers, lr_sched_sd):
-                    lr_sched.load_state_dict(lr_sched_sd)
+
+        # lr schedulers
+        if "lr_schedulers" in state:
+            for lr, lr_sd in zip(self.lr_schedulers, state["lr_schedulers"]):
+                lr.load_state_dict(lr_sd)
+
         if "rng" in state:
             set_rng_state(state["rng"])
